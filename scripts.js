565f4c3e3b562a5a5e85d501f8a4a02ec4913739
Fernando Diego Martins Coelho <fernandomartinsdk@gmail.com>
2023-06-01 22:04:20 -0300

    fist commit

---
+++ scripts.js
@@ -0,0 +1,34 @@
+
+const todosApp = {
+    data(){
+        return {
+            todos: [],
+            newTodo: {
+                done: false
+            }
+        }
+    },methods:{
+        addTodo:function(){
+            if (this.newTodo.text) {
+                this.todos.push(this.newTodo);
+                this.newTodo = {
+                    done:false
+                };
+                localStorage.setItem('todos',JSON.stringify(this.todos))
+            }else{
+                alert('campo vazio')
+            }
+        },
+        storeTodos(){
+            localStorage.setItem('todos',JSON.stringify(this.todos));
+        }
+    },
+    created(){
+        this.todos = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : this.todos;
+    }/*,
+    updated(){
+        localStorage.setItem('todos',JSON.stringify(this.todos));
+    }*/
+};
+
+Vue.createApp(todosApp).mount('#app');
